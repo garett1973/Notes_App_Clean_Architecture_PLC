3749901f1f1c799b1ae069c63f78f906f108600b
garett1973 <hobby@virgis.net>
2023-01-14 13:51:58 +0200

    Implemented AddEditNoteViewModel

--- app/src/main/java/net/virgis/tutorials/notesapp/di/AppModule.kt
+++ app/src/main/java/net/virgis/tutorials/notesapp/di/AppModule.kt
@@ -9,10 +9,7 @@ import dagger.hilt.components.SingletonComponent
 import net.virgis.tutorials.notesapp.feature_note.data.data_source.NoteDatabase
 import net.virgis.tutorials.notesapp.feature_note.data.repository.NoteRepositoryImpl
 import net.virgis.tutorials.notesapp.feature_note.domain.repository.NoteRepository
-import net.virgis.tutorials.notesapp.feature_note.domain.use_case.AddNote
-import net.virgis.tutorials.notesapp.feature_note.domain.use_case.DeleteNote
-import net.virgis.tutorials.notesapp.feature_note.domain.use_case.GetNotes
-import net.virgis.tutorials.notesapp.feature_note.domain.use_case.NoteUseCases
+import net.virgis.tutorials.notesapp.feature_note.domain.use_case.*
 import javax.inject.Singleton
 
 
@@ -42,7 +39,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
